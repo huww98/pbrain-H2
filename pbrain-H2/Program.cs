00f4237586f4ebfcde38715bbaddb8825820dfab
胡玮文 <huww98@outlook.com>
2018-11-03 01:16:35 +0800

    添加项目文件。

---
+++ pbrain-H2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pbrain_H2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
